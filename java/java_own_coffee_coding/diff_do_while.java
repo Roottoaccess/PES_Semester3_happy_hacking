3f530c095586d37d08a321c7555e8642ed71ec4c
Roottoaccess <network123crab@gmail.com>
2025-08-27 15:29:20 +0530

    difference between while and do while loop.....

---
+++ java/java_own_coffee_coding/diff_do_while.java
@@ -0,0 +1,26 @@
+public class diff_do_while{
+
+	public static void main(String[]args){
+
+		int i = 12; // Initializing the value of `i`
+
+		// Applying the while loop concept first
+
+		while(i <= 11){
+			
+			System.out.print(i + " from while loop ");
+			i++;
+		}
+
+
+		// Applying the do while loop concept second
+
+		do{
+			System.out.print(i + " from do while loop ");
+			i++;
+
+		}while(i <= 11);
+	}
+}
+
+// Program terminated.....
