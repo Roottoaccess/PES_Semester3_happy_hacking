6f824856262d6292e64c5a1bf5b672dec26e5758
Roottoaccess <network123crab@gmail.com>
2025-08-29 08:52:01 +0530

    basic input of java....

---
+++ java/collage_java_codes/basic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class basic{
+	public static void main(String[]args){
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the integer: ");
+		int x = sc.nextInt();
+		
+		System.out.println("The integer you have entered is: "+x);
+	}
+}
+// Program terminated.....
